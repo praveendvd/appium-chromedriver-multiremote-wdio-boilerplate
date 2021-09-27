c81a1a2c31b04dbcc295d1e4763fa557a98f50df
praveendvd <45095911+praveendvd@users.noreply.github.com>
2021-09-27 22:12:56 +0100

    Update executeAndOpenReport.bat

--- executeAndOpenReport.bat
+++ executeAndOpenReport.bat
@@ -1,6 +1,3 @@
-CALL rmdir /Q /S allure-results
-CALL rmdir /Q /S allure-report
 CALL npm install
 CALL npx wdio run wdio.conf.js
-CALL node_modules\allure-commandline\dist\bin\allure generate
-CALL node_modules\allure-commandline\dist\bin\allure open
+
